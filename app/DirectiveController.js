2a46c1303d5cf9a2441c15765c113ce336497a7d
Gaurav <max.gaurav@ithands.net>
2015-04-28 00:10:01 +0530

    directives slide completed

--- app/DirectiveController.js
+++ app/DirectiveController.js
@@ -2,19 +2,74 @@
 
 demo.controller('DirectiveController', function ($scope, $modal) {
 
-  var home = this;
-  home.scope = $scope;
+  var directive = this;
+  directive.scope = $scope;
+
+  directive.scope.text2 = 'This is pre loaded data';
+  directive.scope.checkbox2 = true;
+  directive.scope.select2 = 1;
+  directive.scope.radio2 = 1;
+  directive.scope.names = ['John', 'Jessie'];
+  directive.scope.childs = [{
+    'name': 'John',
+    'gender': 'Male'
+  }, {
+    'name': 'Jessie',
+    'gender': 'Fe-Male'
+  }];
+
+  directive.scope.option_values = [{name: 'Value 1'}, {name: 'Value 2'}];
 
   //funtion to open css pop up
-  home.scope.showCss = function () {
+  directive.scope.showCss = function () {
     var modal = {};
     modal.showCss = true;
     modal.showJs = false;
     modal.showHtml = false;
 
     var modalInstance = $modal.open({
-      templateUrl: 'views/info/homeinfo.html',
-      controller: 'HomeController',
+      templateUrl: 'views/info/directiveinfo.html',
+      controller: 'directiveController',
+      size: 'lg',
+      resolve: {
+        items: function () {
+          return modal;
+        }
+      }
+    });
+  };
+
+  directive.scope.addSelect = function (name) {
+    var item = {};
+    item.name = name;
+    directive.scope.option_values.push(item);
+    directive.scope.input_name = '';
+  };
+
+  directive.scope.addName = function (name) {
+
+    directive.scope.names.push(name);
+    directive.scope.input_name = '';
+  };
+
+  directive.scope.addNameObject = function (name, gender) {
+    var item = {};
+    item.name = name;
+    item.gender = gender;
+    directive.scope.childs.push(item);
+    directive.scope.input_name1 = '';
+  };
+
+  //funtion to open css pop up
+  directive.scope.showJs = function () {
+    var modal = {};
+    modal.showCss = true;
+    modal.showJs = false;
+    modal.showHtml = false;
+
+    var modalInstance = $modal.open({
+      templateUrl: 'views/info/directiveinfo.html',
+      controller: 'directiveController',
       size: 'lg',
       resolve: {
         items: function () {
@@ -25,15 +80,15 @@ demo.controller('DirectiveController', function ($scope, $modal) {
   };
 
   //funtion to open css pop up
-  home.scope.showJs = function () {
+  directive.scope.showHtml = function () {
     var modal = {};
     modal.showCss = true;
     modal.showJs = false;
     modal.showHtml = false;
 
     var modalInstance = $modal.open({
-      templateUrl: 'views/info/homeinfo.html',
-      controller: 'HomeController',
+      templateUrl: 'views/info/directiveinfo.html',
+      controller: 'directiveController',
       size: 'lg',
       resolve: {
         items: function () {
@@ -43,26 +98,7 @@ demo.controller('DirectiveController', function ($scope, $modal) {
     });
   };
 
-  //funtion to open css pop up
-  home.scope.showHtml = function () {
-    var modal = {};
-    modal.showCss = true;
-    modal.showJs = false;
-    modal.showHtml = false;
-
-    var modalInstance = $modal.open({
-      templateUrl: 'views/info/homeinfo.html',
-      controller: 'HomeController',
-      size: 'lg',
-      resolve: {
-        items: function () {
-          return modal;
-        }
-      }
-    });
-  };
-
-  home.scope.no = function () {
+  directive.scope.no = function () {
     $modalInstance.dismiss('cancel');
   };
 });
